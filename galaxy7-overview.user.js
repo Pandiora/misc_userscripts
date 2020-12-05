bffa50aaa4ae526700822470815d237be78dbc71
Pandi <Pandiora@users.noreply.github.com>
2020-12-05 22:51:57 +0100

    Misc tweask
    - include oneclick-fleet
    - show indicators for capturing fleets
    - show own and other planets

--- galaxy7-overview.user.js
+++ galaxy7-overview.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Galaxy 7 - Overview
 // @namespace    http://tampermonkey.net/
-// @version      0.14
+// @version      0.20
 // @description  Galaxy 7 - Overview
 // @author       Pandi
 // @updateURL    https://github.com/Pandiora/misc_userscripts/raw/master/galaxy7-overview.user.js
@@ -64,17 +64,33 @@ const bsu = (() => {
     const appendDatasets = async() => {
 
         const typeLen = data.typeDisplay.length;
-        const galaDatasets = await GM.getValue('galaData');
+        const isThisYou = jQuery('.name_palnet:contains("Galaxy7") + span + span').map(function(e){ return this.innerHTML; }).get();
+        const captureData = await getFleetStatus();
+        const isInFlight = captureData[0];
+        galaData = await GM.getValue('galaData');
         let dataTables = "";
 
         for(let i=0;i<typeLen;i++){
-            const obj = (galaDatasets || []).filter(x => x.type == data.typeDisplay[i]);
+            const obj = (galaData || []).filter(x => x.type == data.typeDisplay[i]);
             if(obj.length < 1) continue;
             let pre = `<table><tr><th colspan="4" style="color: ${data.typeColors[data.typeDisplay[i]]};">Type ${data.typeDisplay[i]} (${obj.length})</th></tr><tr><th>Position</th><th>Ally</th><th>Occupied</th><th>Actions</th></tr>`;
 
             for(let j=0;j<obj.length;j++){
                 let occupied = (obj[j].occ == "Yes" || obj[j].ally) ? "Yes" : "No";
-                pre += `<tr><td><a href="${data.baseUrl}game.php?page=galaxy&galaxy=${obj[j].gal}&system=${obj[j].sys}" target="_blank">[${obj[j].gal}:${obj[j].sys}:${obj[j].pos}]</a></td><td>${obj[j].ally}</td><td>${occupied}</td><td><a href="#" onclick="OpenPopup('${data.baseUrl}game.php?page=phalanx&galaxy=${obj[j].gal}&system=${obj[j].sys}&planet=${obj[j].pos}&planettype=1', '', 640, 510);" class="bsu-phalanx" title="Phalanx"></a><a href="${data.baseUrl}game.php?page=fleetTable&galaxy=${obj[j].gal}&system=${obj[j].sys}&planet=${obj[j].pos}&planettype=1&target_mission=25" target="_blank" class="bsu-fleet" title="Send Fleet"></a><a href="" target="_blank" class="bsu-refresh" title="Refresh this dataset"></a></td></tr>`;
+                let direct_fleet = [obj[j].gal,obj[j].sys,obj[j].pos,data.typeDisplay[i]];
+                if(occupied == "Yes"){
+                    for(let k=0;k<isThisYou.length;k++){
+                        const compare = '['+obj[j].gal+':'+obj[j].sys+':'+obj[j].pos+']';
+                        if(isThisYou[k] === compare) occupied = "Own";
+                    }
+                } else if(occupied == "No"){
+                    for(let k=0;k<isInFlight.length;k++){
+                        const timeContainer = captureData[1][k];
+                        const compare = '['+obj[j].gal+':'+obj[j].sys+':'+obj[j].pos+']';
+                        if(isInFlight[k] === compare) occupied = '<a href="#" id="'+timeContainer.id+'" data-fleet-end-time="'+$(timeContainer).data("fleet-end-time")+'" data-fleet-time="'+$(timeContainer).data("fleet-time")+'" class="bsu-fleet-transit fleets"></a>';
+                    }
+                }
+                pre += `<tr><td><a href="${data.baseUrl}game.php?page=galaxy&galaxy=${obj[j].gal}&system=${obj[j].sys}" target="_blank">[${obj[j].gal}:${obj[j].sys}:${obj[j].pos}]</a></td><td>${obj[j].ally}</td><td>${occupied}</td><td><a href="#" onclick="OpenPopup('${data.baseUrl}game.php?page=phalanx&galaxy=${obj[j].gal}&system=${obj[j].sys}&planet=${obj[j].pos}&planettype=1', '', 640, 510);" class="bsu-phalanx" title="Phalanx"></a><a href="${data.baseUrl}game.php?page=fleetTable&galaxy=${obj[j].gal}&system=${obj[j].sys}&planet=${obj[j].pos}&planettype=1&target_mission=25" target="_blank" class="bsu-fleet" title="Send Fleet"></a><a href="#" class="bsu-direct-fleet" title="Send capturing fleet with one click" data-fleet="${direct_fleet}"></a><a href="" target="_blank" class="bsu-refresh" title="Refresh this dataset"></a></td></tr>`;
             }
             dataTables += pre+'</table>';
         }
@@ -83,9 +99,7 @@ const bsu = (() => {
     };
 
     const addButton = async() => {
-
         const button = jQuery('#munu_galaxy + a .imgovernuovo');
-
         jQuery('#munu_galaxy + a').attr("href", "#");
         jQuery(button).css('opacity', '1');
    };
@@ -120,6 +134,14 @@ const bsu = (() => {
 
     };
 
+    const getFleetStatus = async() => {
+        const res = await fetchingData(data.baseUrl+'game.php?game.php?page=overview', [], 'GET'),
+              oD = document.implementation.createHTMLDocument('virtual'),
+              fleets = jQuery(res, oD).find('.holding.ownseizure .ownseizure:last-child').map(function(e){ return this.innerHTML; }).get(),
+              timebase = jQuery(res, oD).find('.holding.ownseizure').parent().prev().children('.fleets').map(function(e){ return this; }).get();
+        return [fleets,timebase];
+    };
+
     const getGalaDataset = async(gal,sys) => {
 
         let res = await fetchingData(data.baseUrl+'game.php?page=galaxy&galaxy='+gal+'&system='+sys, [], 'GET');
@@ -132,8 +154,8 @@ const bsu = (() => {
                   att = (jQuery('.gal_player_cont .ico_post', ref).attr('title')) ? 'Yes' : 'No';
 
             rows.push({
-                gal: gal,
-                sys: sys,
+                gal: parseInt(gal),
+                sys: parseInt(sys),
                 pos: parseInt(jQuery('.gal_number', ref).text().replace(/\s+/g,'')),
                 occ: att,
                 planet: jQuery('.gal_planet_name', ref).text(),
@@ -143,6 +165,14 @@ const bsu = (() => {
         }
 
         return rows;
+    };
+
+    const getSinglePlanet = async(gal,sys,pla) => {
+
+        const sysData = await getGalaDataset(gal,sys),
+              alterGalas = galaData.map(obj => sysData.find(o => o.sys == obj.sys && o.pos == obj.pos) || obj);
+        await GM.setValue('galaData', alterGalas);
+
     };
 
     const getUserConfig = async() => {
@@ -161,17 +191,6 @@ const bsu = (() => {
         for(let i=0;i<len;i++){
             $('#'+data.typeDisplay[i].repeat(2), document).prop('checked', true);
         }
-    };
-
-    const mainHtmlTemplate = () => {
-
-        const html = `
-        <style type="text/css">
-@import"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";.fa-spinner{display:none}#wrapper{height:100%;width:100%;color:white}#bsu-nav{height:30px;padding:10px}button{height:27px;cursor:pointer;float:left;background-color:#006598;border:1px #00ccff solid;color:#FFF;border-radius:4px;margin:0 2px;font-size:16px;box-sizing:border-box;display:block}table{background-color:#000d20;float:left;width:auto !important}td,th{background-color:#091d2e}form{margin-right:20px}table,td,th{border:1px solid black}table tr:first-child th{padding:5px}table tr td,table tr th{padding:2px 5px;text-align:left}a[class^="bsu-"]{height:20px;width:20px;display:inline-block;background-repeat:no-repeat;background-position:center center}.bsu-phalanx{background-image:url('https://static.battlestaruniverse.com/media/gamemedia/styles/images/iconav/radar.png')}.bsu-fleet{background-image:url('https://static.battlestaruniverse.com/media/gamemedia/styles/images/iconav/target.png')}.bsu-refresh{background-image:url('https://static.battlestaruniverse.com/media/gamemedia/styles/images/iconav/office.png')}form #saveTypes,form #multiselect{float:right}#oneillAmount,#startingSystem,#multiselect{height:27px;font-size:16px;font-weight:bold;border-radius:4px;border:1px solid black;color:white;cursor:pointer;background-color:#091d2e;box-sizing:border-box}#oneillAmount + button,#startingSystem + button{float:right}.selectBox{height:100%;position:relative;background-color:#091d2e}.selectBox select{width:100%;height:100%;font-weight:bold;background-color:#091d2e;color:white}.overSelect{position:absolute;left:0;right:0;top:0;bottom:0}#checkboxes{display:none;border:1px #dadada solid;background-color:#091d2e;z-index:1;position:relative;color:white}#checkboxes label{display:block}#checkboxes label:hover{background-color:#1e90ff}#system{width:195px;float:right}#oneill{width:270px;float:right}
-        </style>
-<div id="bsu-nav"> <button id="refresh-results"><i class="fa fa-spinner fa-spin"></i>Refresh Results</button><form> <button id="saveTypes">Save Types</button><div id="multiselect"><div class="selectBox"> <select><option>Select Types</option></select><div class="overSelect"></div></div><div id="checkboxes"> <label for="11"><input type="checkbox" id="11" />Type 1</label> <label for="22"><input type="checkbox" id="22" />Type 2</label> <label for="33"><input type="checkbox" id="33" />Type 3</label> <label for="44"><input type="checkbox" id="44" />Type 4</label> <label for="55"><input type="checkbox" id="55" />Type 5</label> <label for="66"><input type="checkbox" id="66" />Type 6</label> <label for="77"><input type="checkbox" id="77" />Type 7</label> <label for="88"><input type="checkbox" id="88" />Type 8</label> <label for="99"><input type="checkbox" id="99" />Type 9</label></div></div></form><form id="system"> <input id="startingSystem" type="number" step="50" min="1" max="3001"> <button id="saveSystem">Save System</button></form><form id="oneill"> <input id="oneillAmount" type="number" step="1" min="1" max="1000000000000"> <button id="saveOneill">Save Oneill</button></form></div><div id="wrapper"></div>
-        `;
-        return html;
     };
 
     const onClicks = (targetid) => {
@@ -193,22 +212,39 @@ const bsu = (() => {
                     e.preventDefault();
                     jQuery('#checkboxes').toggle('visibility');
                     break;
+                case /bsu-direct-fleet/.test(targetClass):
+                    e.preventDefault();
+                    jQuery('.fa-spinner').toggle();
+                    const fleetData = jQuery(e.target).data('fleet').split(',');
+                    await sendFleet(fleetData[0], fleetData[1], fleetData[2], fleetData[3]);
+                    await reloadContent();
+                    jQuery('.fa-spinner').toggle();
+                    break;
+                case /bsu-refresh/.test(targetClass):
+                    e.preventDefault();
+                    let rowSystem = jQuery(e.target).parent().parent();
+                        rowSystem = jQuery('>td>a', rowSystem).text().match(/(\d+)/gm);
+                    jQuery('.fa-spinner').toggle();
+                    await getSinglePlanet(data.starting_galaxy, rowSystem[1], rowSystem[2]);
+                    await reloadContent();
+                    jQuery('.fa-spinner').toggle();
+                    break;
                 case /saveTypes/.test(targetId):
                     e.preventDefault();
-                    const inputTypes = $('#checkboxes input:checked', document).map(function(){ return this.id[0]; }).get();
+                    const inputTypes = jQuery('#checkboxes input:checked', document).map(function(){ return this.id[0]; }).get();
                     if(inputTypes.length < 1) return;
                     await GM.setValue('types', inputTypes);
                     await reloadContent();
                     break;
                 case /saveOneill/.test(targetId):
                     e.preventDefault();
-                    const inputOneills = $('#oneillAmount', document).val();
+                    const inputOneills = jQuery('#oneillAmount', document).val();
                     if(inputOneills.length < 1) return;
                     await GM.setValue('oneills', inputOneills);
                     break;
                 case /saveSystem/.test(targetId):
                     e.preventDefault();
-                    const inputSystem = $('#startingSystem', document).val();
+                    const inputSystem = jQuery('#startingSystem', document).val();
                     if(inputSystem.length < 1) return;
                     await GM.setValue('systems', inputSystem);
                     break;
@@ -224,11 +260,6 @@ const bsu = (() => {
         });
     };
 
-    const updateSingleEntry = (coords) => {
-
-
-    };
-
     const reloadContent = async () => {
         await getUserConfig();
         const dataSets = await appendDatasets();
@@ -236,9 +267,34 @@ const bsu = (() => {
         setUserConfig();
     };
 
-    const sendFleet = (coords) => {
+    const sendFleet = async(gal,sys,pos,type) => {
 
+        let fetchData = [
+            { 'galaxy': gal, 'system': sys, 'planet': pos, 'type': type, 'target_mission': 25, 'ship221': data.battleShipAmount, 'save_groop': '' },
+            { 'galaxy': gal, 'system': sys, 'planet': pos, 'type': type, 'token': '', 'fleet_group': 0, 'target_mission': 25, 'TIMING': '5.568415100487793', 'speed': 10 },
+            { 'token': '', 'groupAttackMOD': 0, 'mission': 25, 'metal': 0, 'crystal': 0, 'deuterium': 0, 'staytime': 0 }
+        ];
 
+        let token = await fetchingData(data.baseUrl+'game.php?page=fleetStep1', fetchData[0], 'POST');
+        token = jQuery(token).find('input[name=token]').eq(0).val();
+        fetchData[1].token = fetchData[2].token = token;
+
+        await fetchingData(data.baseUrl+'game.php?page=fleetStep2', fetchData[1], 'POST');
+        await fetchingData(data.baseUrl+'game.php?page=fleetStep3', fetchData[2], 'POST').then(res => {
+            const oD = document.implementation.createHTMLDocument('virtual'),
+                  txt = jQuery(res, oD).find('.ally_contents').text();
+            console.log('If this result is empty, your fleet got send: '+txt);
+        });
+    };
+
+    const mainHtmlTemplate = () => {
+        const html = `
+        <style type="text/css">
+        @import"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";.fa-spinner{display:none}#wrapper{height:100%;width:100%;color:white}#bsu-nav{height:30px;padding:10px}button{height:27px;cursor:pointer;float:left;background-color:#006598;border:1px #00ccff solid;color:#FFF;border-radius:4px;margin:0 2px;font-size:16px;box-sizing:border-box;display:block}table{background-color:#000d20;float:left;width:auto !important}td,th{background-color:#091d2e}form{margin-right:20px}table,td,th{border:1px solid black}table tr:first-child th{padding:5px}table tr td,table tr th{padding:2px 5px;text-align:left}a[class^="bsu-"]:hover{filter: contrast(0.5);}a[class^="bsu-"]{height:20px;width:20px;display:inline-block;background-repeat:no-repeat;background-position:center center}.bsu-phalanx{background-image:url('https://static.battlestaruniverse.com/media/gamemedia/styles/images/iconav/radar.png')}.bsu-fleet{background-image:url('https://static.battlestaruniverse.com/media/gamemedia/styles/images/iconav/target.png')}.bsu-direct-fleet{background-image:url('https://static.battlestaruniverse.com/media/gamemedia/styles/images/iconav/hangar.png');}.bsu-fleet-transit{background-image:url('https://static.battlestaruniverse.com/media/gamemedia/styles/images/iconav/fleet_fleet.gif');}.bsu-refresh{background-image:url('https://static.battlestaruniverse.com/media/gamemedia/styles/images/iconav/office.png')}form #saveTypes,form #multiselect{float:right}#oneillAmount,#startingSystem,#multiselect{height:27px;font-size:16px;font-weight:bold;border-radius:4px;border:1px solid black;color:white;cursor:pointer;background-color:#091d2e;box-sizing:border-box}#oneillAmount + button,#startingSystem + button{float:right}.selectBox{height:100%;position:relative;background-color:#091d2e}.selectBox select{width:100%;height:100%;font-weight:bold;background-color:#091d2e;color:white}.overSelect{position:absolute;left:0;right:0;top:0;bottom:0}#checkboxes{display:none;border:1px #dadada solid;background-color:#091d2e;z-index:1;position:relative;color:white}#checkboxes label{display:block}#checkboxes label:hover{background-color:#1e90ff}#system{width:195px;float:right}#oneill{width:270px;float:right}.fleets{padding: 0 0 0 20px !important;line-height: 21px !important;background-position-x: left !important;width: 45px !important;}
+        </style>
+        <div id="bsu-nav"> <button id="refresh-results" title="Update all results (will take some time)"><i class="fa fa-spinner fa-spin"></i>Update</button><form> <button id="saveTypes">Save Types</button><div id="multiselect"><div class="selectBox"> <select><option>Select Types</option></select><div class="overSelect"></div></div><div id="checkboxes"> <label for="11"><input type="checkbox" id="11" />Type 1</label> <label for="22"><input type="checkbox" id="22" />Type 2</label> <label for="33"><input type="checkbox" id="33" />Type 3</label> <label for="44"><input type="checkbox" id="44" />Type 4</label> <label for="55"><input type="checkbox" id="55" />Type 5</label> <label for="66"><input type="checkbox" id="66" />Type 6</label> <label for="77"><input type="checkbox" id="77" />Type 7</label> <label for="88"><input type="checkbox" id="88" />Type 8</label> <label for="99"><input type="checkbox" id="99" />Type 9</label></div></div></form><form id="system"> <input id="startingSystem" type="number" step="50" min="1" max="3001"> <button id="saveSystem">Save System</button></form><form id="oneill"> <input id="oneillAmount" type="number" step="1" min="1" max="1000000000000"> <button id="saveOneill">Save Oneill</button></form></div><div id="wrapper"></div>
+        `;
+        return html;
     };
 
     const randu = (min, max) => {
@@ -250,7 +306,6 @@ const bsu = (() => {
     };
 
     return {
-        updateSingleEntry,
         addButton,
         onClicks,
         init,
